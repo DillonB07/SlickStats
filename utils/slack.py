059487a3032b1eabab59cd85cd18ef7f9bc32c5d
Dillon Barnes <dillonb07dev@gmail.com>
2024-10-08 18:16:26 +0100

    don't set empty status if status is empty

--- utils/slack.py
+++ utils/slack.py
@@ -56,6 +56,10 @@ def update_slack_status(emoji, status, user_id, token, expiry=0):
     emojis = [e.get("emoji") for e in STATUSES]
 
     if status_emoji in emojis or status_emoji == "":
+        current_status_text = current_status["profile"].get("status_text", "")
+        print(current_status_text)
+        if current_status_text == status:
+            return
         app.client.users_profile_set(
             user=user_id,
             profile={
